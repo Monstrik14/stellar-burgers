5fc2994704133a2b6e77b4668ff0e34766e19585
Sergei Suprun <sergeysuprun@list.ru>
2025-03-13 20:39:23 +0300

    Update ingredientsSlice.tsx

--- src/services/slices/ingredientsSlice.tsx
+++ src/services/slices/ingredientsSlice.tsx
@@ -1,11 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { TIngredient } from "@utils-types";
+import { getIngredientsApi } from "@api";
+
+
 
 export interface IngredientDetails {
-  ingredients: [],
-  isLoading: false,
+  ingredients: TIngredient[],
+  isLoading: boolean,
   bun: {
     price: 0
-  }
+  }, 
+  error: string | null
 }
 
 export const initialState: IngredientDetails = {
@@ -13,15 +18,35 @@ export const initialState: IngredientDetails = {
   isLoading: false,
   bun: {
     price: 0
+  },
+  error: null
+}
+
+export const fetchIngredients = createAsyncThunk (
+  'ingredients/fetchIngredients',
+  async () => {
+    const ingredients = await getIngredientsApi();
+    return ingredients;
   }
-};
+)
 
 export const ingredientsSlice = createSlice({
   name: 'ingredients',
   initialState,
-  reducers: {
-    setIngredients: (state, action) => {
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchIngredients.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(fetchIngredients.fulfilled, (state, action: PayloadAction<TIngredient[]>) => {
       state.ingredients = action.payload;
+      state.isLoading = false;
+    })
+    .addCase(fetchIngredients.rejected, (state) => {
+      state.isLoading = false;
+      state.error = 'error';
+    })
     },
   },
-});
+);
