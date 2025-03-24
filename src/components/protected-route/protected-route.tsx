abd608cc3223973d359bff6b2c29235840c68854
Sergei Suprun <sergeysuprun@list.ru>
2025-03-24 19:15:19 +0300

    24/03

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,49 +1,11 @@
-// import { useSelector } from '../../services/store';
-// import { Navigate, useLocation } from 'react-router';
-// import {
-//   isAuthCheckedSelector,
-//   loginUserRequestSelector
-// } from '../../slices/userSlice';
-// import { Preloader } from '../ui/preloader';
-// import { useEffect } from 'react';
-
-// type ProtectedRouteProps = {
-//   onlyUnAuth?: boolean;
-//   children: React.ReactElement;
-// };
-
-// export const ProtectedRoute = ({
-//   onlyUnAuth,
-//   children
-// }: ProtectedRouteProps) => {
-//   const isAuthChecked = useSelector(isAuthCheckedSelector);
-//   const loginUserRequest = useSelector(loginUserRequestSelector);
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     console.log('ProtectedRoute: isAuthChecked =', isAuthChecked);
-//     console.log('ProtectedRoute: loginUserRequest =', loginUserRequest);
-//   }, [isAuthChecked, loginUserRequest]);
-
-//   if (!isAuthChecked && loginUserRequest) {
-//     return <Preloader />;
-//   }
-
-//   if (!onlyUnAuth && !isAuthChecked) {
-//     return <Navigate replace to='/login' state={{ from: location }} />; // в
-//   }
-
-//   if (onlyUnAuth && isAuthChecked) {
-//     const from = location.state?.from || { pathname: '/' };
-//     return <Navigate replace to={from} state={location} />;
-//   }
-//   return children;
-// };
-
 import { useSelector } from '../../services/store';
 import { Navigate, useLocation } from 'react-router';
-import { isAuthCheckedSelector, authChecked } from '../../slices/userSlice';
+import {
+  isAuthCheckedSelector,
+  loginUserRequestSelector
+} from '../../slices/userSlice';
 import { Preloader } from '../ui/preloader';
+import { useEffect } from 'react';
 
 type ProtectedRouteProps = {
   onlyUnAuth?: boolean;
@@ -55,21 +17,26 @@ export const ProtectedRoute = ({
   children
 }: ProtectedRouteProps) => {
   const isAuthChecked = useSelector(isAuthCheckedSelector);
-  const isAuthenticated = useSelector(authChecked);
+  const loginUserRequest = useSelector(loginUserRequestSelector);
   const location = useLocation();
 
-  if (!isAuthChecked) {
+  useEffect(() => {
+    console.log('ProtectedRoute: isAuthChecked =', isAuthChecked);
+    console.log('ProtectedRoute: loginUserRequest =', loginUserRequest);
+  }, [isAuthChecked, loginUserRequest]);
+
+  if (!isAuthChecked && loginUserRequest) {
     return <Preloader />;
   }
 
-  if (!onlyUnAuth && !isAuthenticated) {
-    return <Navigate replace to='/login' state={{ from: location }} />;
+  if (!onlyUnAuth && !isAuthChecked) {
+    return <Navigate replace to='/login' state={{ from: location }} />; // в
   }
 
-  if (onlyUnAuth && isAuthenticated) {
-    const from = location.state?.from || '/';
-    return <Navigate replace to={from} />;
+  if (onlyUnAuth && isAuthChecked) {
+    const from = location.state?.from || { pathname: '/' };
+    return <Navigate replace to={from} state={location} />;
   }
-
   return children;
 };
+
